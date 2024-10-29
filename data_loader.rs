a129bb8c7158e5fba7311f5a6ec1734c146d29d7
mihai9909 <neagoimihai@gmail.com>
2024-10-29 10:37:12 +0200

    Return vector of tuples from load_dataset and create distance function

--- data_loader.rs
+++ data_loader.rs
@@ -11,19 +11,19 @@ fn img_to_vec(path: String) -> Vec<u8> {
   img_gray.into_raw()
 }
 
-pub fn load_train_dataset() -> Vec<Vec<u8>> {
-  let mut all_images: Vec<Vec<u8>> = Vec::new();
+pub fn load_train_dataset() -> Vec<(Vec<u8>,u8)> {
+  let mut all_images: Vec<(Vec<u8>,u8)> = Vec::new();
   for i in 0..10 {
-    let path = format!("MNIST/{}/", i);
-    let images: Vec<Vec<u8>> = load_images(&path);
+    let path = format!("MNIST/train/{}/", i);
+    let images: Vec<(Vec<u8>,u8)> = load_images(&path);
     all_images.extend(images);
   }
   println!("Number of images: {}", all_images.len());
   all_images
 }
 
-pub fn load_images(directory_path: &str) -> Vec<Vec<u8>> {
-  let mut images: Vec<Vec<u8>> = vec![];
+pub fn load_images(directory_path: &str) -> Vec<(Vec<u8>, u8)> {
+  let mut images: Vec<(Vec<u8>, u8)> = vec![];
 
   match fs::read_dir(directory_path) {
     Ok(entries) => {
@@ -34,7 +34,7 @@ pub fn load_images(directory_path: &str) -> Vec<Vec<u8>> {
             // println!("{:?}", file_name);
             let mut path_to_file = directory_path.to_string(); // Start with a mutable String
             path_to_file.push_str(&file_name.to_string_lossy()); // Append the file name
-            images.push(img_to_vec(path_to_file));
+            images.push((img_to_vec(path_to_file), 10));
           }
           Err(e) => eprintln!("Error reading entry: {}", e),
         }
@@ -46,15 +46,15 @@ pub fn load_images(directory_path: &str) -> Vec<Vec<u8>> {
   images
 }
 
-fn print_pixel_data(pixels: Vec<u8>) {
-  for (i, pixel) in pixels.iter().enumerate() {
-    print!("{} ", pixel);
-    if i % 28 == 0 {
-      println!();
-    }
-  }
-}
+// fn print_pixel_data(pixels: Vec<u8>) {
+//   for (i, pixel) in pixels.iter().enumerate() {
+//     print!("{} ", pixel);
+//     if i % 28 == 0 {
+//       println!();
+//     }
+//   }
+// }
 
-fn print_vec_len(pixels: Vec<u8>) {
-  println!("Grayscale pixel data length: {}", pixels.len());
-}
+// fn print_vec_len(pixels: Vec<u8>) {
+//   println!("Grayscale pixel data length: {}", pixels.len());
+// }
--- main.rs
+++ main.rs
@@ -1,19 +1,16 @@
 mod data_loader;
 
 fn main() {
-  // let pixels: Vec<u8> = img_to_vec("MNIST/0/1.png");
+  let images: Vec<(Vec<u8>, u8)> = data_loader::load_train_dataset(); // vector of tuples containing the image as vector and the class of the image (default 10)
+  let distance: f32 = distance(&images[0].0, &images[40000].0);
+  println!("distance: {}", distance);
+}
 
-  // print_pixel_data(pixels);
-
-  // let path = "MNIST/0/";
-  // let mut images: Vec<Vec<u8>> = load_images("MNIST/0/");
-  // println!("Number of images: {}", images.len());
-  // for image in images {
-  //   print_vec_len(image);
-  // }
-  // let random_numbers: Vec<i32> = (0..length)
-  //       .map(|_| rng.gen_range(1..=100)) // Generates random numbers in the range [1, 100]
-  //       .collect();
-  
-  let images: Vec<Vec<u8>> = data_loader::load_train_dataset();
+fn distance(a: &Vec<u8>, b: &Vec<u8>) -> f32 {
+  let mut sum: f32 = 0.0;
+  for i in 0..a.len() {
+    let diff: f32 = (a[i] as f32 - b[i] as f32).into();
+    sum += (diff)*(diff);
+  };
+  f32::sqrt(sum)
 }
